b6827fc2e7df4492f3ee0ef346fab96424d6fca0
peter <peter@orchid2>
2019-05-22 21:04:30 -0600

    try again

--- Hello/src/HelloGit.java
+++ Hello/src/HelloGit.java
@@ -1,10 +1,9 @@
-package testpkg;
 
-public class Main {
+public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Git");
+
 	}
 
 }
