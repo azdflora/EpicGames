72f15269e77d909a971d33a10f469dd539e9718b
azdflora <azdflora@gmail.com>
2019-05-22 21:48:18 -0600

    try more this time

--- Hello/src/code/HelloGit.java
+++ Hello/src/code/HelloGit.java
@@ -1,9 +1,10 @@
+package code;
 
 public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Git");
+		System.out.print("Hello Git\n");
 	}
 
 }
